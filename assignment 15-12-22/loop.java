36c31387e6ce1117fc6de707c4595bc36f8e9592
Priyakar00 <120620773+Priyakar00@users.noreply.github.com>
2022-12-15 13:49:41 +0530

    Create loop.java

---
+++ assignment 15-12-22/loop.java
@@ -0,0 +1,14 @@
+public class loop{
+	public static void main(String[]args){
+	int i;
+	for(i=0; i<5; i++)
+	{
+		System.out.println("Hello");
+	}
+	System.out.println();
+	for(i=5; i>0; i--)
+	{
+		System.out.println("world");
+	}
+}
+}
