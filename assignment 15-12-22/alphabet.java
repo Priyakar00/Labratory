f29401c5d26e5dd956680b16b7ed6aed071dbce6
Priyakar00 <120620773+Priyakar00@users.noreply.github.com>
2022-12-15 13:50:41 +0530

    Create alphabet.java

---
+++ assignment 15-12-22/alphabet.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class alphabet{
+	public static void main(String[]args){
+	Scanner sc= new Scanner(System.in);
+	char c;
+	System.out.println("Enter a character:");
+	c= sc.next().charAt(0);
+	if((c>='a' && c<='z')||(c>='A' && c<='Z'))
+	{
+		System.out.println("The character is an alphabet");
+	}
+	else
+	{
+		System.out.println("The character is not an alphabet");
+	}
+}
+}
