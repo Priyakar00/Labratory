a5f6900daa5e8a352488425d47fc92660b2a0e81
Priya Karmakar <120620773+Priyakar00@users.noreply.github.com>
2023-01-12 14:32:49 +0530

    Update Java.java

--- 12-01-23/BatchApplication/Java.java
+++ 12-01-23/BatchApplication/Java.java
@@ -1,1 +1,58 @@
+package batchApplication;
+
+public class Java {
+      
+	private int stdId;
+	private String stdName;
+	private long phone;
+	private String email;
+	
+	public Java() {
+		super();
+	}
+
+	
+	public Java(int stdId, String stdName, long phone, String email) {
+		super();
+		this.stdId = stdId;
+		this.stdName = stdName;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+
+	public int getStdId() {
+		return stdId;
+	}
+
+	public void setStdId(int stdId) {
+		this.stdId = stdId;
+	}
+
+	public String getStdName() {
+		return stdName;
+	}
+
+	public void setStdName(String stdName) {
+		this.stdName = stdName;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
 
